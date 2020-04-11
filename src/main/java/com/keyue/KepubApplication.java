6fb24ad1c897481acba6a76776e98e07d2b54b1d
luojp <luojp@chromium.org>
2020-04-11 23:21:31 +0800

    遗漏文件

---
+++ src/main/java/com/keyue/KepubApplication.java
@@ -0,0 +1,13 @@
+package com.keyue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KepubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KepubApplication.class, args);
+	}
+
+}
