427e1e68b5f5d714bd25529596bf3d9e6c7f2321
luojp <luojp@chromium.org>
2020-04-13 18:58:35 +0800

    遗漏文件

---
+++ src/test/java/com/keyue/KepubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KepubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
